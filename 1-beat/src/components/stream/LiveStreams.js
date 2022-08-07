ca46dd59da5d175da6e3c00db88c138e8324bcfd
Akash <akash.lamprostech@gmail.com>
2022-08-07 13:31:01 +0530

    Prasanna's Commit

---
+++ 1-beat/src/components/stream/LiveStreams.js
@@ -0,0 +1,224 @@
+import React from "react";
+import "../stream/LiveStream.scss";
+import Livepeer from "livepeer-nodejs";
+
+function LiveStreams() {
+  const livepeerObject = new Livepeer("d72d5808-9b46-4bdf-9cb6-d703ca3e0acc");
+  const live = async (e) => {};
+  return (
+    <>
+      <div className="livestream-main-container">
+        <div className="livestream-header">
+          <h1 className="livestream-t-header">LiveStream</h1>
+          <p className="livestream-p-header">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            temporibus sed voluptatem dicta quas vitae quibusdam omnis similique
+            optio sint esse quisquam
+          </p>
+        </div>
+        <div className="livestream-main-content">
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="stream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+
+          <div className="livestream-content">
+            <div className="livestream-img">
+              <a>
+                <img src="https://picsum.photos/200" alt="" />
+              </a>
+              <div class="btn btn_live">
+                Live<span class="live-icon"></span>
+              </div>
+            </div>
+            <div className="livestream-title">
+              <h4>The news you asking thought, king’s</h4>
+            </div>
+            <div className="livestream-user">
+              <div className="livestream-img-name">
+                <span>user name</span>
+              </div>
+            </div>
+            <div className="livestream-time">
+              <span>1 year ago</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LiveStreams;
